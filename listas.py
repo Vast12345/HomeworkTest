35a18a150f465abf0525f00094db0c753d8d3721
vast <vaztplayz1>
2023-09-01 18:58:34 -0500

    feat: :sparkles: test
    Test for list commands

---
+++ listas.py
@@ -0,0 +1,27 @@
+lista = ["hello", 2, True, 2.22]
+
+print(lista)
+
+for palabra in lista:
+    print (palabra)
+
+
+print("---------------------------------------------------------------")
+lista.append("Perro")
+
+for word in lista:
+    print (word)
+
+print("---------------------------------------------------------------------")
+
+lista_input = int(input())
+
+lista.append(lista_input)
+
+for word in lista:
+    print(word)
+
+
+
+
+
